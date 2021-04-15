9dc82de88c0d25f8fb484173db4a2710bc3b4858
Nitin Singhal <nitinsinghal139@gmail.com>
2021-04-15 21:46:15 +0530

    Add user crud operations

--- crudapp/src/main/java/com/springboot/novoice/service/UserService.java
+++ crudapp/src/main/java/com/springboot/novoice/service/UserService.java
@@ -1,4 +1,41 @@
 package com.springboot.novoice.service;
 
+import com.springboot.novoice.entity.User;
+import com.springboot.novoice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean delete(Long id) {
+        Optional<User> optional = userRepository.findById(id);
+        if(optional.isPresent()) {
+            userRepository.delete(optional.get());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean update(User user) {
+        Optional<User> optional = userRepository.findById(user.getId());
+        if(optional.isPresent()) {
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
 }
