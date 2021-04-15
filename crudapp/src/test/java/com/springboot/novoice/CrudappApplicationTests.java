9dc3b34921b9623ad6516c7bdb7ec197707153ee
Nitin Singhal <nitinsinghal139@gmail.com>
2021-04-15 21:08:11 +0530

    Initial commit

---
+++ crudapp/src/test/java/com/springboot/novoice/CrudappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.novoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
