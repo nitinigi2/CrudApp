74579c83a29f4717de36d79e248a890e94c29d93
Nitin Singhal <nitinsinghal139@gmail.com>
2021-04-17 18:52:41 +0530

    change postrepository class to interafce

--- crudapp/src/main/java/com/springboot/novoice/repository/PostRepository.java
+++ crudapp/src/main/java/com/springboot/novoice/repository/PostRepository.java
@@ -1,4 +1,11 @@
 package com.springboot.novoice.repository;
 
-public class PostRepository {
+import com.springboot.novoice.entity.Post;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findByUserId(Long userId);
+
 }
