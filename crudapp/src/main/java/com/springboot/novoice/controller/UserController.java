8d950a64e1d2cfdb535fb789bbf99912e59f3401
Nitin Singhal <nitinsinghal139@gmail.com>
2021-04-18 21:47:56 +0530

    Add @valid annotation for user creation

--- crudapp/src/main/java/com/springboot/novoice/controller/UserController.java
+++ crudapp/src/main/java/com/springboot/novoice/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -21,7 +22,7 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<?> addUser(@RequestBody User user){
+    public ResponseEntity<?> addUser(@Valid @RequestBody User user){
         if(user != null) {
             User savedUser = userService.save(user);
             URI uri = ServletUriComponentsBuilder.
